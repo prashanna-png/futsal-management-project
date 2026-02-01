b74ba0f0b7f4737ef105a8ceb7ad7510a13a7819
prashanna <prashanna9742@gmail.com>
2026-02-01 22:34:22 +0545

    add rating feature

--- data/venue.js
+++ data/venue.js
@@ -3,21 +3,24 @@ export const venueData = [
         image: "down-town.webp",
         name: "Downtown Sports Arena",
         address: "Chyasal, lalitpur",
-        availableCourts: 4,
-        bookingPrice: 500
+        bookingPrice: 500,
+        contact: 9876543210,
+        rating: { stars: 4.5 }
     },
     {
         image: "up-town.jpg",
         name: "Uptown Futsal Center",
         address: "Imadol, lalitpur",
-        availableCourts: 6,
-        bookingPrice: 750
+        bookingPrice: 750,
+        contact: 9123456780,
+        rating: { stars: 3.0 }
     },
     {
         image: "city-side.jpg",
         name: "Cityside Futsal Hub",
         address: "Baneshwor, Kathmandu",
-        availableCourts: 5,
-        bookingPrice: 600
+        bookingPrice: 600,
+        contact: 9234567890,
+        rating: { stars: 5 }
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,14 @@ let venueHtml = '';
 venueData.forEach(venue => {
   venueHtml += `
     <div class="venue-card">
-      <img src="images/${venue.image}" alt="${venue.name}" width="100%" />
+      <img class="venue-image" src="images/${venue.image}" alt="${venue.name}" width="100%" />
       <h3>${venue.name}</h3>
       <p>${venue.address}</p>
-      <p>Available Courts: ${venue.availableCourts}</p>
       <p>Booking Price: Rs. ${venue.bookingPrice}/hour</p>
+      <p>Contact: ${venue.contact}</p>
+      <div class="product-rating-container">
+        <img class="product-rating-stars" src="images/ratings/rating-${venue.rating.stars * 10}.png">
+      </div>
       <button>View Details</button>
     </div>
   `;

658c90deec67caf60dcc6c7f1fc6444f60027b9c
prashanna <prashandon97@gmail.com>
2026-06-29 21:50:59 +0545

    add db.php

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = 'localhost';
+$database = 'futsal_management_system';
+$username = 'root';
+$password = 'prashan@2005';
+
+$conn = mysqli_connect($hostname, $username, $password,$database);
+
+if(!$conn){
+  die(''. mysqli_connect_error());
+}
+?>

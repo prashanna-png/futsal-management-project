556a1fb5200ff5ff7eeb77116f73f9d491deedac
prashanna <prashandon97@gmail.com>
2026-07-19 18:44:06 +0545

    init landing page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+  <link rel="stylesheet" href="../assets/css/landing-page.css">
+</head>
+
+<body>
+  <nav class="nav-bar">
+    <div class="left-section">
+      FutsalHub
+    </div>
+    <div class="center-section">
+      <a href="">Home</a>
+      <a href="">Features</a>
+      <a href="">How it works</a>
+      <a href="">Pricing</a>
+      <a href="">FAQ</a>
+    </div>
+    <div class="right-section">
+      <button>Sign in</button>
+      <button>Login</button>
+    </div>
+  </nav>
+
+</body>
+
+</html>
